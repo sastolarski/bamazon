b8b3c0e3c5f01a4b4339bc20d92c9fd49e10bc27
sastolarski <sastolarski@gmail.com>
2018-06-22 15:34:14 -0500

    connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,6 +18,17 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    connection.end();
+    afterConnection();
    
   });
+
+  function afterConnection(){
+    connection.query("SELECT * FROM products", function(err, res){
+      if(err) throw err;
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
+      }
+      
+    })
+    connection.end();
+  } 
