20b153185db60fbb883d3e7bd3d33ffa74b6a7a8
sastolarski <sastolarski@gmail.com>
2018-06-22 15:04:28 -0500

    beginning stuff

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE bamazon;
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(60) NOT NULL,
+    department_name VARCHAR(60),
+    price INT,
+    stock_quanitity INT,
+    PRIMARY KEY (item_id)
+);
+
