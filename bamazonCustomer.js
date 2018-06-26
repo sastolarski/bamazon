02ad097309d9cb1d8eea6253a50d1b0c4788985f
sastolarski <sastolarski@gmail.com>
2018-06-26 16:01:24 -0500

    inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,33 +2,61 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "root",
-  database: "bamazon"
+    // Your password
+    password: "root",
+    database: "bamazon"
 });
 
+//On connect to database run first function
+//===================================================================
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    afterConnection();
-   
-  });
+    start();
 
-  function afterConnection(){
-    connection.query("SELECT * FROM products", function(err, res){
-      if(err) throw err;
-      for (var i = 0; i < res.length; i++) {
-        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
-      }
-      
+});
+
+//start function uses inquirier to ask user if they would like to see the inventory
+//===================================================================
+function start() {
+    inquirer
+        .prompt({
+            name: "purchase",
+            type: "rawlist",
+            message: "Would you like to purchase an item from our inventory? [YES] or [NO]",
+            choices: ["YES", "NO"]
+        })
+        .then(function (answer) {
+            // based on their answer, either call the bid or the post functions
+            if (answer.purchase.toUpperCase() === "YES") {
+                showInventory();
+            }
+            else {
+                console.log("nothing")
+            }
+        });
+}
+//second function shows inventory if user confirms the first function
+//===================================================================
+
+function showInventory() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
+        }
+
     })
     connection.end();
-  } 
+}
+
+
+
